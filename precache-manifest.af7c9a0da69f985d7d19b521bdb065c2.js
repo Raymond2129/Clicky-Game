80045a2acbb6feafa0377e2eca74b2851fbfefb9
Ray Ray <raymond2129@gmail.com>
2019-04-08 13:20:42 -0600

    Updates

---
+++ precache-manifest.af7c9a0da69f985d7d19b521bdb065c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "d10b840fc054549509a4",
+    "url": "/clicky-game/static/js/main.cd920cd2.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d10b840fc054549509a4",
+    "url": "/clicky-game/static/css/main.2dc55fd7.chunk.css"
+  },
+  {
+    "revision": "e57ae8fd696d34730c1b1c613300964c",
+    "url": "/clicky-game/index.html"
+  }
+];
